da3aedc743bb013b7981f21b2ad3e89c9204d946
Mor89 <wilson0001.1111@gmail.com>
2022-05-05 18:34:12 -0600

    Creacion de Constructor y Get y Set.

---
+++ Commit/src/main/java/com/mycompany/commit/Suma.java
@@ -0,0 +1,38 @@
+
+package com.mycompany.commit;
+
+
+public class Suma {
+    int a = 0;
+    int b = 0;
+    int total;
+
+    public Suma() {
+    }
+
+    public Suma(int total) {
+        this.total = total;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    
+}
