1c5a57806b278e27e6e9361d4e202baa57e3b2d3
Mor89 <wilson0001.1111@gmail.com>
2022-05-05 18:48:51 -0600

    Creacion de Metodo

---
+++ Commit/src/main/java/com/mycompany/commit/MainSuma.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.commit;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Wilso
+ */
+public class MainSuma {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        Suma suma = new Suma();
+        System.out.println("Introdusca el primer Numero");
+        suma.setA(leer.nextInt());
+        
+        System.out.println("Introdusca la segunda sifra");
+        suma.setB(leer.nextInt());
+        System.out.println("-----------------------------");
+    }
+}
--- Commit/src/main/java/com/mycompany/commit/Suma.java
+++ Commit/src/main/java/com/mycompany/commit/Suma.java
@@ -33,6 +33,10 @@ public class Suma {
     public int getTotal() {
         return total;
     }
-
+   
+    int suma (){
+        total = a + b;
+        return total;
+    }
     
 }
